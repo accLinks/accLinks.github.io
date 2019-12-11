18922b307eaa61f27440969cba4f97a3e0acc355
accLinks <57611814+accLinks@users.noreply.github.com>
2019-12-10 23:12:43 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function hide(){
-	console.log('hey');
+
+var userAdmin = createUser("admin", "codeStudioVisual", "adminRank");
+var userGuest = createUser("guest", "guest12Password", "guestRank");
+var userList = [userAdmin, userGuest];//list of users
+//document.getElementById('logInPageId').classList.toggle('hidden');
+function logInFunction(){
+    
+    const userInputUsername = document.getElementById('usernameHome').value;//username input box
+    const userInputPassword = document.getElementById('passwordHome').value;//password input box
+    if(userInputUsername.length > 0 || userInputPassword.length > 0){
+        for(let i = 0; i < userList.length; i++){//array that goes through user list
+        //userList[i].username == userInputUsername && userList[i].password == userInputPassword
+            if(userList[i].username == userInputUsername && userList[i].password == userInputPassword){
+                document.getElementById('logInPageId').classList.toggle('hidden');
+                document.getElementById('loggedOnPageId').classList.toggle('hidden');
+                document.getElementById('loggedOnPageWelcome').textContent = `Welcome ${userList[i].username}, feel free to look around.`;  
+            }  
+        }
+    }else if(document.getElementById('loggedOnPageWelcome').textContent != "Error 0"){
+        document.getElementById('loggedOnPageId').classList.toggle('hidden');
+        document.getElementById('loggedOnPageWelcome').textContent = "Error 0";
+    }
+    
+}
+function createUser(username, password, userRank){
+    return {username, password, userRank};
 }
 
-
-//ss
+  
+  
